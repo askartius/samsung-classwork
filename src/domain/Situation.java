90b115e7466c932f5f19350dc9b94ab62f0a5400
Askartius <151680098+askartius@users.noreply.github.com>
2023-12-07 20:39:16 +0300

    OOP

---
+++ src/domain/Situation.java
@@ -0,0 +1,16 @@
+package domain;
+
+public class Situation {
+    public Situation[] direction;
+    public String subject, text;
+    public int dK, dA, dR;
+
+    public Situation(String subject, String text, int variants, int dk, int da, int dr) {
+        this.subject = subject;
+        this.text = text;
+        dK = dk;
+        dA = da;
+        dR = dr;
+        direction = new Situation[variants];
+    }
+}
