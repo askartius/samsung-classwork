4b17a7d8aab088992ccefbd9a10f37bcc1d10048
Askartius <151680098+askartius@users.noreply.github.com>
2023-12-20 22:23:23 +0300

    File input/output stream

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
-        
+        File file = new File("data.txt");
+
+        FileInputStream input = null;
+        FileOutputStream output = null;
+
+        try {
+            input = new FileInputStream("C:\\Users\\askar\\Documents\\Coding\\IJ_Projects\\Samsung\\Classwork\\src\\input.txt");
+            output = new FileOutputStream("C:\\Users\\askar\\Documents\\Coding\\IJ_Projects\\Samsung\\Classwork\\src\\output.txt");
+
+            int c;
+            while((c = input.read()) != -1) {
+                output.write(c);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
