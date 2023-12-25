d9a8e7d17e96c7a52dadb0f67f48d886e661a189
Askartius <151680098+askartius@users.noreply.github.com>
2023-12-25 23:28:33 +0300

    Class inside Main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,22 @@ import domain.Parcel;
 
 public class Main {
     public static void main(String[] args) {
-        Parcel parcel = new Parcel();
-        Parcel.Destination destination = parcel.new Destination();
+        class Test {
+            private int a;
+            private int b;
+
+            public Test(int a, int b) {
+                this.a = a;
+                this.b = b;
+            }
+
+            public int sum() {
+                return a + b;
+            }
+        }
+
+        Test test = new Test(2, 4);
+        System.out.println(test.sum());
+
     }
 }
