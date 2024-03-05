7411adda2c96477c5be64027c803414cade80e68
Askartius <151680098+askartius@users.noreply.github.com>
2024-03-05 20:40:57 +0300

    Tree Set

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import domain.Person;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-        MyBinaryTree tree = new MyBinaryTree(10);
+        Set<Person> treeSet = new TreeSet<>(new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getAge() - o2.getAge();
+            }
+        });
+        treeSet.add(new Person(1L, "Valera", 16));
+        treeSet.add(new Person(1L, "Vitya", 15));
+
+        System.out.println(treeSet);
     }
 }
---
+++ src/domain/Person.java
@@ -0,0 +1,39 @@
+package domain;
+
+public class Person implements Comparable {
+    private long id;
+    private String name;
+    private int age;
+
+    public Person(long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
