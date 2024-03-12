330448c657d8fd1c4d7bc42cfe75ccd587c35d0a
Askartius <151680098+askartius@users.noreply.github.com>
2024-03-12 20:20:31 +0300

    Custom Set

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,15 @@
-import domain.Person;
-
-import java.util.*;
+import set.Set100Int;
 
 public class Main {
     public static void main(String[] args) {
-        Person[] people = new Person[5];
-        people[0] = new Person(1L, "Andy", 1);
-        people[1] = new Person(2L, "Andrew", 5);
-        people[2] = new Person(3L, "Matt", 15);
-        people[3] = new Person(4L, "Peter", 8);
-        people[4] = new Person(5L, "Carl", 4);
-        /*Arrays.sort(people, new Comparator<Person>() {
-            @Override
-            public int compare(Person o1, Person o2) {
-                return o1.getAge() - o2.getAge();
-            }
-        });*/
-        Arrays.sort(people, 1, 4);
+        Set100Int set100Int = new Set100Int();
 
-        ArrayList<Person> personList = new ArrayList<>();
-        personList.add(new Person(1L, "Andy", 1));
-        personList.add(new Person(2L, "Andrew", 5));
-        personList.add(new Person(3L, "Matt", 15));
-        personList.add(new Person(4L, "Peter", 8));
-        personList.add(new Person(5L, "Carl", 4));
-        Collections.sort(personList);
+        set100Int.add(77);
+        set100Int.add(5);
+        set100Int.add(-10);
+        set100Int.add(100);
 
-        System.out.println(personList);
+        System.out.println(set100Int.contains(1));
+        System.out.println(set100Int.contains(5));
     }
 }
